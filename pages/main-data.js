efeeaa799094ab47d41f473cf4accf383a4a1d31
Josh Jenkin <43326100+jjenks713@users.noreply.github.com>
2022-01-25 17:08:58 -0700

    first commit

---
+++ pages/main-data.js
@@ -0,0 +1,163 @@
+export const mainData = [
+        {
+            "url": "/images/harlots-calamaty.jpg",
+            "name": "Harlots Calamity",
+            "med": "Intaglio on Stonehenge",
+            "size": "10”X14”",
+            "ed": "Ed. 15",
+            "val": 1
+        },
+        {
+            "url": "/images/heres-johnny.jpg",
+            "name": "Heres Johnny",
+            "med": "Screenprint on Stonehenge",
+            "size": "14”X18”",
+            "ed": "Ed. 15",
+            "val": 2
+        },
+        {
+            "url": "/images/human-nature.jpg",
+            "name": "Human Nature",
+            "med": "Relief on Stonehenge",
+            "size": "20”x15”",
+            "ed": "Ed. 10",
+            "val": 3
+        },
+        {
+            "url": "/images/2020.jpg",
+            "name": "2020",
+            "med": "Relief on Masa",
+            "size": "7”x10”",
+            "ed": "Ed. 9",
+            "val": 4
+        },
+        {
+            "url": "/images/a-town-divided.jpg",
+            "name": "A Town Divided",
+            "med": "Suicide Relief on Lenox",
+            "size": "11”x15”",
+            "ed": "Ed. 6",
+            "val": 5
+        },
+        {
+            "url": "/images/alice.jpg",
+            "name": "Alice",
+            "med": "Screenprint on Stonehenge",
+            "size": "11”x18”",
+            "ed": "Ed. 10",
+            "val": 6
+        },
+        {
+            "url": "/images/any-other-name.jpg",
+            "name": "Any Other Name",
+            "med": "Intaglio on Lenox",
+            "size": "6”x8”",
+            "ed": "Ed. 20",
+            "val": 7
+        },
+        {
+            "url": "/images/happy-family.jpg",
+            "name": "Happy Family",
+            "med": "Intaglio on Rives BFK",
+            "size": "11”x14”",
+            "ed": "Ed. 10",
+            "val": 8
+        },
+        {
+            "url": "/images/king-of-printers.jpg",
+            "name": "King of Printers",
+            "med": "Letterpress on Lettra",
+            "size": "5.25”x7”",
+            "ed": "Ed. 50",
+            "val": 9
+        },
+        {
+            "url": "/images/marionette.jpg",
+            "name": "Marionette",
+            "med": "Intaglio on Stonehenge",
+            "size": "10”x14”",
+            "ed": "Ed. 15",
+            "val": 10
+        },
+        {
+            "url": "/images/monument.jpg",
+            "name": "Monument Valley",
+            "med": "Screenprint on Stonehenge",
+            "size": "12”x12”",
+            "ed": "Ed. 20",
+            "val": 11
+        },
+        {
+            "url": "/images/psychopaths.jpg",
+            "name": "Psychopaths",
+            "med": "Letterpress on Lettra",
+            "size": "5.5”x9.5”",
+            "ed": "Ed. 25",
+            "val": 12
+        },
+        {
+            "url": "/images/purgatory.jpg",
+            "name": "Psychopaths",
+            "med": "Screenprint on Stonehenge",
+            "size": "11”x14”",
+            "ed": "Ed. 7",
+            "val": 13
+        },
+        {
+            "url": "/images/sinking.jpg",
+            "name": "Sinking",
+            "med": "Photopolymer Etching on Stonehenge",
+            "size": "5”x7”",
+            "ed": "Ed. 20",
+            "val": 14
+        },
+        {
+            "url": "/images/sisters-bw.jpg",
+            "name": "Sisters B&W",
+            "med": "Relief on Masa",
+            "size": "5”x6”",
+            "ed": "Ed. 30",
+            "val": 15
+        },
+        {
+            "url": "/images/sisters.jpg",
+            "name": "Sisters",
+            "med": "Relief on Masa",
+            "size": "5”x6”",
+            "ed": "Ed. 30",
+            "val": 16
+        },
+        {
+            "url": "/images/sloth.jpg",
+            "name": "Sloth",
+            "med": "Suicide Relief and Letterpress",
+            "size": "11”x13.5”",
+            "ed": "Ed. 25",
+            "val": 17
+        },
+        {
+            "url": "/images/studio-life.jpg",
+            "name": "Studio Life",
+            "med": "Suicide Relief on Masa",
+            "size": "12”x12”",
+            "ed": "Ed. 10",
+            "val": 18
+        },
+        {
+            "url": "/images/tenacious.jpg",
+            "name": "Tenacious",
+            "med": "Intaglio on Stonehenge",
+            "size": "8”x10”",
+            "ed": "Ed. 10 AP",
+            "val": 19
+        },
+        {
+            "url": "/images/unawares.jpg",
+            "name": "Unawares",
+            "med": "Relief and Watercolor on Lenox",
+            "size": "12”x16”",
+            "ed": "Ed. 5",
+            "val": 20
+        }
+      ]
+ 
