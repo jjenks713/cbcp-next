9b69529a0e1b0cd07b49bd6a00fd723a31a56938
Josh Jenkin <43326100+jjenks713@users.noreply.github.com>
2022-01-27 10:38:12 -0700

    css on Navigator, attempted bg pic layout still not working.

---
+++ components/commission-data.js
@@ -0,0 +1,114 @@
+export const commissionData = [
+        {
+            url: '/images/Rita.jpg',
+            name: 'Rita',
+            med: 'Copic Markers and Gel Pens',
+            size: '6"X8in"',
+            val: '1'
+        },
+        {
+            url: '/images/Nyx.jpg',
+            name: 'Nyx',
+            med: 'Copic Markers and Gel Pens',
+            size: '6"X8in"',
+            val: '2'
+        },
+        {
+            url: '/images/bear.png',
+            name: 'Bear',
+            med: 'Wood Burning on Cutting Board',
+            size: '',
+            val: '3'
+        },
+        {
+            url: '/images/boar.png',
+            name: 'Boar',
+            med: 'Wood Burning',
+            size: '24”x36”',
+            val: '4'
+        },
+        {
+            url: '/images/for-hideo.jpg',
+            name: 'For hideo',
+            med: 'Copic Markers and Gel Pens',
+            size: '8”x10”',
+            val: '5'
+        },
+        {
+            url: '/images/girl-1.jpg',
+            name: 'Girl 1',
+            med: 'Charcoal on Gray Paper',
+            size: '9”x11”',
+            val: '6'
+        },
+        {
+            url: '/images/girl-2.jpg',
+            name: 'Girl 2',
+            med: 'Charcoal on Gray Paper',
+            size: '9"x11"',
+            val: '7'
+        },
+        {
+            url: '/images/girl-3.jpg',
+            name: 'Girl 3',
+            med: 'Charcoal on Gray Paper',
+            size: '9”x11”',
+            val: '8'
+        },
+        {
+            url: '/images/heart.png',
+            name: 'Heart',
+            med: 'Oil Painting',
+            size: '18”x24”',
+            val: '9'
+        },
+        {
+            url: '/images/i-love-you.jpeg',
+            name: 'I love You',
+            med: 'Oil Painting',
+            size: '',
+            val: '10'
+        },
+        {
+            url: '/images/img.jpg',
+            name: 'Spidey Venom',
+            med: 'Acrylic Painting',
+            size: '16”x20”',
+            val: '11'
+        },
+        {
+            url: '/images/jose.jpg',
+            name: 'Jose',
+            med: 'Copic Markers and Gel Pens',
+            size: '8”x10”',
+            val: '12'
+        },
+        {
+            url: '/images/miles.jpg',
+            name: 'Miles',
+            med: 'Copic Markers and Gel Pens',
+            size: '8”x10”',
+            val: "13"
+        },
+        {
+            url: '/images/mothers-love.jpg',
+            name: 'Mothers Love',
+            med: 'Pastels and Ink',
+            size: '18”x14”',
+            val: "15"
+        },
+        {
+            url: '/images/the-godfather.jpg',
+            name: 'The Godfather',
+            med: 'Acrylic Painting',
+            size: '12”x16”',
+            val: "16"
+        },
+        {
+            url: '/images/Yue.jpg',
+            name: 'Yue',
+            med: '',
+            size: '',
+            val: "17"
+        },
+    ];
---
+++ components/navigator.js
@@ -0,0 +1,131 @@
+import React from "react";
+import Head from 'next/head'
+import Link from 'next/link'
+import { Menu } from '@headlessui/react'
+
+export default function Navigator() {
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
+  return (
+    <>
+    <Head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    </Head>
+      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-zinc mb-3">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-center">
+          <div className="w-full relative flex flex-wrap justify-center lg:w-auto lg:static lg:block lg:justify-start">
+            <Link href="/"> 
+            <img //eslint-disable-line
+              src="./images/CBCP-Long.png" 
+              className="hover:opacity-75 cursor-pointer"
+            >
+            </img>
+            </Link>
+
+            <button
+              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <i className="fa fa-angle-down"></i>
+            </button>
+          </div>
+          <div
+            className={
+              "lg:flex items-center flex-wrap" +
+              (navbarOpen ? " flex" : " hidden")
+            }
+          >
+            <ul className="flex flex-wrap list-none lg:ml-auto py-3 md:text-sm">
+
+            <li className="nav-item">
+            <Menu>
+              <Menu.Button className="px-3 py-2 flex items-center text-md uppercase font-bold leading-snug text-white hover:opacity-75 bg-gray-900">Prints <i className="fa fa-angle-down"></i></Menu.Button>
+              <Menu.Items>
+
+
+                <Menu.Item className="px-3 py-2 flex items-center text-md uppercase font-bold leading-snug text-white hover:opacity-75 bg-gray-900">
+                  {({ active }) => (
+                    <a // eslint-disable-line
+                      className={`${active && 'bg-blue-500'}`}
+                      as={Link}
+                      href="/"
+                    >
+                      Prints
+                    </a>
+
+
+                  )}
+                </Menu.Item>
+
+                <Menu.Item className="px-3 py-2 flex items-center text-md uppercase font-bold leading-snug text-white hover:opacity-75 bg-gray-900">
+                  {({ active }) => (
+                    <a // eslint-disable-line
+                      className={`${active && 'bg-blue-500'}`}
+                      as={Link}
+                      href="/books"
+                    >
+                      Artist Books
+                    </a>
+                  )}
+                </Menu.Item>
+              </Menu.Items>
+            </Menu>
+            </li>
+
+              <li className="nav-item">
+                <Link href='/drawings'>
+                  <a
+                  className="px-3 py-2 flex items-center text-md uppercase font-bold leading-snug text-white hover:opacity-75"
+                  
+                >
+                  Drawings +
+                </a></Link>
+
+              </li>
+
+              <li className="nav-item">
+              <Link href="/commissions">
+                <a
+                  className="px-3 py-2 flex items-center text-md uppercase font-bold leading-snug text-white hover:opacity-75"
+                >
+                  Commissions
+                </a>
+                </Link>
+              </li>
+              
+              <li className="nav-item">
+                <a // eslint-disable-line
+                  className="px-3 py-2 flex items-center text-md uppercase font-bold leading-snug text-white hover:opacity-75"
+                  href="https://www.etsy.com/shop/creepybutcutepress"
+                  target="_blank"
+                >
+                  Shop
+                </a>
+              </li>
+              
+              <li className="nav-item">
+              <Link href="/about">
+                <a
+                  className="px-3 py-2 flex items-center text-md uppercase font-bold leading-snug text-white hover:opacity-75"
+                >
+                  About
+                </a>
+                </Link>
+              </li>
+              
+              <li className="nav-item">
+              <Link href="/contact">
+                <a
+                  className="px-3 py-2 flex items-center text-md uppercase font-bold leading-snug text-white hover:opacity-75"
+                >
+                  Contact
+                </a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
