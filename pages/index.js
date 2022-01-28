702bfc2c78693c1aa5e07c3af8e5155ae0cc8363
Josh Jenkin <43326100+jjenks713@users.noreply.github.com>
2022-01-28 15:21:46 -0700

    modals on index pages working with data and images

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,25 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { mainData }  from '../components/main-data';
 import Navigator from '../components/navigator';
-//import Layout from '../components/layouts';
+import CircularProgress from '../components/loader'
+import * as React from 'react';
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 export async function getStaticProps() {
 
@@ -19,7 +37,14 @@ export async function getStaticProps() {
 
 export default function Home({mainPhotos}) {
 
-//console.log(mainPhotos);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false);
+  const [modalPhotos, setModalData] = useState('');
+
+  console.log(modalPhotos);
+
+
 
   return (
     <>
@@ -30,6 +55,8 @@ export default function Home({mainPhotos}) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navigator></Navigator>
+
+      {/* <CircularProgress></CircularProgress> */}
       <div className="grid justify-center px-3 py-10 font-Nanum">
         <h1 className="uppercase font-bold leading-snug text-black"
         style={{
@@ -42,22 +69,39 @@ export default function Home({mainPhotos}) {
       <div className="p-10 masonry sm:masonry-sm md:masonry-md lg:masonry-lg">
         {mainPhotos.map(mainPhoto => (
               <div className='hover:opacity-50 p-3 break-inside drop-shadow-xl' key={mainPhoto.id}>
-                <a href={"/"}>
+                <a onClick={() => {
+                  handleOpen();
+                  setModalData([mainPhoto.ed, mainPhoto.med, mainPhoto.name, mainPhoto.size, mainPhoto.url])
+                }}>
                 <img // eslint-disable-line
                 src={mainPhoto.url} 
                 alt="image"
                 />
                 </a>
-
-              {/*<Link href={"/"}>
-                <Image src={mainPhoto.url} alt="image" width={200} height={200}></Image>
-                </Link>
-                <ul>
-                  <li>Name: {mainPhoto.name}</li>
-                  <li>Medium: {mainPhoto.med}</li>
-                  <li>Size: {mainPhoto.size}</li>
-                  <li>Edition: {mainPhoto.ed}</li>
-                </ul> */}
+                  <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                    key={mainPhoto.id}
+                  >
+                    <Box sx={style} key={modalPhotos}>
+                    <a>
+                      <img // eslint-disable-line
+                      src={modalPhotos[4]} 
+                      alt="image"
+                      />
+                      </a>
+                      <Typography id="modal-modal-title" variant="h6" component="h2">
+                        <ul>
+                          <li>Name: {modalPhotos[2]}</li>
+                          <li>Medium: {modalPhotos[1]}</li>
+                          <li>Size: {modalPhotos[3]}</li>
+                          <li>Edition: {modalPhotos[0]}</li>
+                        </ul>                      
+                      </Typography>
+                    </Box>
+                  </Modal>
               </div>
             ))}
 
