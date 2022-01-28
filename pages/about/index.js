692a7a4590ff61110b10e978bda5386772f4bcbc
Josh Jenkin <43326100+jjenks713@users.noreply.github.com>
2022-01-27 19:57:23 -0700

    all pages built, need functionality and css

---
+++ pages/about/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head"
+import Link from 'next/link'
+import Image from 'next/image'
+import Navigator from '../../components/navigator'
+
+export default function About() {
+
+
+  return (
+    <>
+    <Navigator></Navigator>
+      <div>
+      I believe in making a living out of something you love doing. When I was given the ultimate question in school, “What do you want to be when you grow up?” – I had no clue. People kept saying that being an artist was a joke. I knew that I enjoyed drawing, I could sit all day and work on one piece of art. I was excited every time I saw myself improving. That was about the time I decided that I didn’t care what others thought. If I wanted to draw all day, design things, and create portraits, I would make that my career. I am still extremely excited when I see myself improve. I enjoy learning new mediums and techniques. I work very hard to keep producing art, improving with every step. Every day is a struggle, but each struggle leads to a new victory.
+      </div>
+    </>
+  )
+}
