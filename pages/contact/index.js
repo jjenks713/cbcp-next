576477e58ec915e63af451d8f873e1103cc01257
Josh Jenkin <43326100+jjenks713@users.noreply.github.com>
2022-03-11 14:40:44 -0700

    contact page css

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -5,7 +5,7 @@ export default function About() {
 
     return (
         <>
-        <div className="grid justify-center px-3 py-10 font-Nanum">
+        <div className="grid justify-center px-10 py-10 font-Nanum">
             <h1 className="uppercase font-bold leading-snug text-black"
                 style={{
                     fontSize: '40px'
@@ -14,18 +14,18 @@ export default function About() {
 
         </div>
         <div className="font-Nanum text-logo grid justify-center uppercase font-bold text-center">
-        <h1 className='text-2xl'>Send us an email at <a href="mailto:creepybutcutepress@gmail.com" className='text-center'>creepybutcutepress@gmail.com</a></h1>
-        <small className='text-black'>
+        <h1 className='text-xl'>Send us an email at <a href="mailto:creepybutcutepress@gmail.com" className='text-center'>creepybutcutepress@gmail.com</a></h1>
+        <small className='text-black text-xs'>
         To purchase, please add work title and quantity in message.<br />
         Payments made to Venmo: @Mary-Jenkin<br />
         Free Shipping on any order in the continental US.
         </small>
         <a // eslint-disable-line
-                  className="hover:opacity-75 py-24 text-2xl"
+                  className="hover:opacity-75 py-24 text-xl"
                   href="https://www.etsy.com/shop/creepybutcutepress"
                 >
                   Or check out our Shop
-                  <img src="images/etsy.png" alt="Etsy" />
+                  <img src="images/etsy.png" alt="Etsy" className='px-10' />
         </a>
         </div>
         <Footer />
